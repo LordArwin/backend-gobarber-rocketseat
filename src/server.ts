424f77e5b495bac4fb9689a2b1a7de3d2755ead2
LordArwin <cleitonjunior2607@gmail.com>
2020-12-03 23:33:51 -0400

    configs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-import express from 'express'
+/* eslint-disable no-console */
+import express from 'express';
+import routes from './routes';
 
-const app = express()
-
-app.get('/', (request, response)=>{
-   return response.json({msg:`Hello GoStack`})
-})
-
-app.listen(3333, () => console.log('🤡 running in port 3333'))
+const app = express();
+app.use(express.json());
+app.get('/', (request, response) => response.json({ msg: 'Hello GoStack' }));
+app.listen(3333, () => console.log('🤡 running in port 3333'));
